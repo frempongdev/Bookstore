922998d54bc1f6ef6a7ea2daedc8332c3226679d
Frempong <samankwaa.frempong@gmail.com>
2023-03-14 15:42:16 +0000

    Add bookSlice as a slice to store from /features/bookSlice.js

---
+++ src/redux/features/books/bookSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+});
+
+export default bookSlice.reducer;
