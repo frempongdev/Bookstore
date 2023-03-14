979263d451c6f647b5e9bd49891f0982ce5d9b30
Frempong <samankwaa.frempong@gmail.com>
2023-03-14 17:18:36 +0000

    Add reducers for adding and removing book

---
+++ src/components/booksource.js
@@ -0,0 +1,18 @@
+const bookArray = [
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    authur: 'Suzanne Collins',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    authur: 'Frank Herbert',
+  },
+  {
+    id: 3,
+    title: 'Capital in the 21st Century',
+    authur: 'Suzanne Collins',
+  },
+];
+export default bookArray;
--- src/redux/features/books/bookSlice.js
+++ src/redux/features/books/bookSlice.js
@@ -1,13 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
+import bookArray from '../../../components/booksource';
 
 const initialState = {
-  books: [],
+  books: bookArray,
+  completed: false,
 
 };
 
 const bookSlice = createSlice({
   name: 'book',
   initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      const bookId = action.payload;
+      state.books.filter((book) => book.id !== bookId);
+    },
+  },
 });
+
+export const { addBook, removeBook } = bookSlice.actions;
 
 export default bookSlice.reducer;
