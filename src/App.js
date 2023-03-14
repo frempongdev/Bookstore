d2d2772eda2ff416043de7605c42a36a7bb9aa4e
Frempong <samankwaa.frempong@gmail.com>
2023-03-14 14:51:34 +0000

    Install redux -toolkit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-/* eslint-disable import/extensions */
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Books from './components/Books.js';
-import Categories from './components/Categories.js';
+import Books from './components/Books';
+import Categories from './components/Categories';
 import Navigation from './components/Navigation';
 
 function App() {
