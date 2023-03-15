1075b4a5364eb00c19b3e25ba80d700b490ae0ef
Frempong <samankwaa.frempong@gmail.com>
2023-03-15 18:04:18 +0000

    Add reducer for addBook action and displayed added books

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,14 +1,31 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../redux/books/bookSlice';
 
-const BookForm = () => (
-  <div>
-    <form>
-      <h3>ADD NEW BOOK</h3>
-      <input placeholder="Book Title" type="input" />
-      <input placeholder="Author" type="input" />
-      <input type="submit" value="Add Book" />
-    </form>
-  </div>
-);
+const BookForm = () => {
+  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  return (
+    <div>
+      <form>
+        <h3>ADD NEW BOOK</h3>
+        <input placeholder="Book Title" type="input" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input placeholder="Author" type="input" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <button
+          type="button"
+          onClick={() => {
+            const item_id = books.length + 1;
+            dispatch(addBook({ item_id, title, author }));
+          }}
+        >
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
+  status: 'Under Contruction',
 };
 
 const categoriesSlice = createSlice({
