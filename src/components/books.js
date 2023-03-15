9ccc6ffa137b7b0e1ee14aee485f126dae443901
Frempong <samankwaa.frempong@gmail.com>
2023-03-15 14:34:38 +0000

    Add redux store and button with removeBook action

--- src/components/books.js
+++ src/components/books.js
@@ -1,27 +1,35 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import '../styles/Books.css';
 import BookForm from './BookForm';
-import bookArray from './booksource';
 
-const Books = () => (
-  <div>
+const Books = () => {
+  const {
+    books,
+  } = useSelector((state) => state.books);
+
+  return (
     <div>
-      <ul className="bookList">
-        {bookArray.map((book) => (
-          <li key={book.id}>
-            <Book
-              title={book.title}
-              author={book.author}
-            />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <ul className="bookList">
+          {books.map((book) => (
+            <li key={book.item_id}>
+              <Book
+                title={book.title}
+                author={book.author}
+                id={book.item_id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <BookForm />
+      </div>
     </div>
-    <div>
-      <BookForm />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Books;
