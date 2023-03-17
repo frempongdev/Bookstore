3e0583330aafcc86c66d5ae642eaad2752924577
Frempong <samankwaa.frempong@gmail.com>
2023-03-17 18:58:41 +0000

    Update Book.css to give style to book details

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,21 +8,45 @@ const Book = ({
 }) => {
   const dispatch = useDispatch();
   return (
-    <div className="bookDetails">
-      <span className="bookCategory">{category}</span>
-      <span className="bookTitle">{title}</span>
-      <span className="bookAuthor">{author}</span>
-      <button
-        className="removeBook"
-        type="button"
-        onClick={() => {
-          dispatch(removeBook(id));
-          dispatch(removeBookFromAPI(id));
-        }}
-      >
-        Remove Book
-      </button>
+    <div className="bookContainer">
+      <div className="bookLeft">
+        <div className="bookDetails">
+          <span className="bookCategory">{category}</span>
+          <span className="bookTitle">{title}</span>
+          <span className="bookAuthor">{author}</span>
+        </div>
+        <div className="bookBtns">
+          <button type="button" className="commentBtn">Comment</button>
+          <div className="btnDivider" />
+          <button
+            className="removeBtn"
+            type="button"
+            onClick={() => {
+              dispatch(removeBook(id));
+              dispatch(removeBookFromAPI(id));
+            }}
+          >
+            Remove
+          </button>
+          <div className="btnDivider" />
+          <button type="button" className="editBtn">Edit</button>
+        </div>
+      </div>
+      <div className="bookMid">
+        <div className="progress">
+          <div className="progress-circle" />
 
+          64%
+          <span className="completed">Completed</span>
+        </div>
+      </div>
+      <div className="bookRight">
+        <div className="chapter">
+          <span className="currChapter">CURRENT CHAPTER</span>
+          <span className="currChapter">Chapter 2</span>
+        </div>
+        <button type="button" className="upChapBtn">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 };
