b9a4d232518838c86383c57a28436c88d5090dad
Frempong <samankwaa.frempong@gmail.com>
2023-03-17 21:47:26 +0000

    Update Categories.css to give style to Category Tab

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,13 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categoriesSlice';
+import '../styles/Categories.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
   const { status } = useSelector((state) => state.categories);
   return (
-    <div>
+    <div className="catStatus">
       <p>{status}</p>
-      <button type="button" onClick={() => dispatch(checkStatus())}>
+      <button className="statBtn" type="button" onClick={() => dispatch(checkStatus())}>
         Check Status
       </button>
     </div>
