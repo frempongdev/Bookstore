7468e28ce8f0de6d26b6f7a4c409dc2ab2116b68
Frempong <samankwaa.frempong@gmail.com>
2023-03-17 09:40:55 +0000

    Implement removeBook from API action

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/bookSlice';
+import { removeBook, removeBookFromAPI } from '../redux/books/bookSlice';
 import '../styles/Book.css';
 
 // eslint-disable-next-line camelcase, react/prop-types
@@ -13,15 +13,23 @@ const Book = ({
       <span className="bookCategory">{category}</span>
       <span className="bookTitle">{title}</span>
       <span className="bookAuthor">{author}</span>
-      <button className="removeBook" type="button" onClick={() => { dispatch(removeBook(id)); }}>
-        Remove
+      <button
+        className="removeBook"
+        type="button"
+        onClick={() => {
+          dispatch(removeBook(id));
+          dispatch(removeBookFromAPI(id));
+        }}
+      >
+        Remove Book
       </button>
+
     </div>
   );
 };
 
 Book.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
